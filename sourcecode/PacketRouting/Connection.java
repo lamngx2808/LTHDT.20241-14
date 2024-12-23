27f5f762baa9001ae134c621fda0b389318c6edc
NgLan <ngoclan271204@gmail.com>
2024-12-24 00:26:57 +0700

    Update Node, Connection class & Add ActivePacket Class

--- sourcecode/PacketRouting/Connection.java
+++ sourcecode/PacketRouting/Connection.java
@@ -3,60 +3,60 @@ package PacketRouting;
 import java.util.ArrayList;
 
 public class Connection {
-  private Node node1;
-  private Node node2;
-  private int latency; // the amount of time unit this connection needs to send 1 unit of data from
-                       // this node to the other node.
-  private ArrayList<Packet> packets = new ArrayList<>();
-  private ArrayList<Integer> progress = new ArrayList<>();
-  private ArrayList<Integer> dest = new ArrayList<>();
-  // the 3 array list create a table with 3 cols of packets that is on the connection, the progress will
-  // the time unit it's spent on the connection, if the progress == latency, the packet will be delivered to the
-  // destination node and removed from the connection arrays. the dest array denote whether the destination node is
-  // node 1 or node 2
-  public Connection(Node node1, Node node2, int latency) {
-    super();
-    this.node1 = node1;
-    this.node2 = node2;
-    node1.addConnection(this);
-    node2.addConnection(this);
-    this.latency = latency;
-  }
-
-  public Node getNode1() {
-    return node1;
-  }
-
-  public Node getNode2() {
-    return node2;
-  }
-
-  public int getLatency() {
-    return latency;
-  }
-
-  public void setLatency(int l) {
-    this.latency = l;
-  }
-
-  public void addPacket(Packet packet, Node sender) {
-    packets.add(packet);
-    progress.add(0);
-    if(sender == this.node1) dest.add(1);
-    else dest.add(2);
-  }
-
-  public void tick(){
-    for(int i = 0; i < packets.size(); i++){
-      progress.set(i, progress.get(i)+1);
-      if(progress.get(i) == latency){
-        if(dest.get(i) == 1) node1.addPacket(packets.get(i));
-        else node2.addPacket(packets.get(i));
-        packets.remove(i);
-        progress.remove(i);
-        dest.remove(i);
-        i--;
-      }
-    }
-  }
+	private Node node1;
+	private Node node2;
+	private int latency; // the amount of time unit this connection needs to send 1 unit of data 
+	                     // from this node to the other node.
+	private ArrayList<ActivePacket> activePackets = new ArrayList<ActivePacket>();
+	
+	// Constructor
+	public Connection(Node node1, Node node2, int latency) {
+		super();
+	    this.node1 = node1;
+	    this.node2 = node2;
+	    this.latency = latency;
+	}
+	
+	// Getter and Setter
+	public Node getNode1() {
+	    return node1;
+	}
+	
+	public Node getNode2() {
+	    return node2;
+	}
+	
+	public int getLatency() {
+	    return latency;
+	}
+	
+	public void setLatency(int latency) {
+	    this.latency = latency;
+	}
+	
+	public void addPacket(Packet packet) {
+	    activePackets.add(new ActivePacket(packet));
+	}
+	
+	public void removePacket(ActivePacket packet) {
+		activePackets.remove(packet);
+	}
+	
+	public void tick() {		
+	    for (ActivePacket activePacket : activePackets) {
+	    	if (activePacket.getProgress() == this.latency) {
+	    		// Neu goi tin da den dich thi them vao danh sach cac goi tin nhan dc
+	    		if (activePacket.getPacket().getDestination().getId() == this.node2.getId()) {
+	    			this.node2.addReceivedPacket(activePacket.getPacket());
+	    		} else {
+	    			// Neu goi tin chua den dich thi them goi tin vao list packet de tiep tuc dinh tuyen
+	    			this.node2.addPacket(activePacket.getPacket());
+	    		}
+	    		// Xoa goi tin khoi ds cac goi tin dang truyen trong lien ket hien tai
+	    		this.removePacket(activePacket);
+	    	} else {
+	    		activePacket.incProgress();
+	    	}
+	    }
+	}
 }
--- sourcecode/PacketRouting/Node.java
+++ sourcecode/PacketRouting/Node.java
@@ -4,137 +4,129 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Node { // will always be connecting to a router
-  private static int idCounter = 0; // Static counter for unique IDs
-  private int id;
-  private String name;
-  private String ipAddress;
-  private Connection default_gateway;
+abstract class Node { 
+	private static int idCounter = 0; // Static counter for unique IDs
+	private int id;
+	private String name;
+	private String ipAddress;
+	private String default_gateway;
+	  
+	private ArrayList<Connection> connections = new ArrayList<>();
+	private Queue<Packet> packets = new LinkedList<>();
+	private ArrayList<Packet> receivedPackets = new ArrayList<>();
 
-  // connections contain the connection to immediate node and the routing table contain the connection that is in a path to
-  // the destination node
-  private final ArrayList<RoutingEntry> routingTable = new ArrayList<>();
-  private final ArrayList<Connection> connections = new ArrayList<>();
-  private Queue<Packet> packets = new LinkedList<>();
-  private ArrayList<Packet> receivePackets = new ArrayList<>();
+	// Constructor
+	public Node(String name, String ipAddress) {
+	    super();
+	    this.id = idCounter++;
+	    this.name = name;
+	    this.ipAddress = ipAddress;
+	}
+	
+	public Node(String name, String ipAddress, String default_gateway) {
+		super();
+		this.name = name;
+		this.ipAddress = ipAddress;
+		this.default_gateway = default_gateway;
+	}
 
-  public Node(String name, String ipAddress) {
-    super();
-    this.id = idCounter++;
-    this.name = name;
-    this.ipAddress = ipAddress;
-  }
+	// Getter and Setter
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
-  // Getters and Setters
-  public int getId() {
-    return id;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getIpAddress() {
+		return ipAddress;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
 
-  public String getIpAddress() {
-    return ipAddress;
-  }
+	public String getDefault_gateway() {
+		return default_gateway;
+	}
 
-  public void setIpAddress(String ipAddress) {
-    this.ipAddress = ipAddress;
-  }
+	public void setDefault_gateway(String default_gateway) {
+		this.default_gateway = default_gateway;
+	}
 
-  public Connection getDefault_gateway() {
-    return default_gateway;
-  }
+	public ArrayList<Connection> getConnections() {
+		return connections;
+	}
 
-  public void setDefault_gateway(Connection default_gateway) {
-    this.default_gateway = default_gateway;
-  }
+	public Queue<Packet> getPackets() {
+		return packets;
+	}
 
-  // Routing Table Methods
-  public ArrayList<RoutingEntry> getRoutingTable() {
-    return routingTable;
-  }
-
-  public void addRoutingEntry(RoutingEntry re) {
-    routingTable.add(re);
-  }
-
-  public void addRoutingEntry(ArrayList<RoutingEntry> re) {
-    routingTable.addAll(re);
-  }
-
-  public void removeRoutingEntry(RoutingEntry re) {
-    routingTable.remove(re);
-  }
-
-  public void removeRoutingEntry(ArrayList<RoutingEntry> re) {
-    routingTable.removeAll(re);
-  }
-
-  public void updateRoutingEntry(RoutingEntry pikachu, RoutingEntry mewtwo) {
-    routingTable.remove(pikachu);
-    routingTable.add(mewtwo);
-  }
-
-  // Packet Methods
-  public Queue<Packet> getPackets() {
-    return packets;
-  }
-
-  public void addPacket(Packet packet) {
-    this.packets.add(packet);
-  }
-
-  public void addPacket(ArrayList<Packet> packets) {
-    this.packets.addAll(packets);
-  }
-
-  public void removePacket(Packet packet) {
-    this.packets.remove(packet);
-  }
-
-  public void removePacket(ArrayList<Packet> packets) {
-    this.packets.removeAll(packets);
-  }
-
-  // Connection Methods
-  public ArrayList<Connection> getConnections() {
-    return connections;
-  }
-
-  public void addConnection(Connection connection) {
-    connections.add(connection);
-  }
-
-  public void addConnection(ArrayList<Connection> connections) {
-    this.connections.addAll(connections);
-  }
-
-  public void removeConnection(Connection connection) {
-    connections.remove(connection);
-  }
-
-  public void removeConnection(ArrayList<Connection> connections) {
-    this.connections.removeAll(connections);
-  }
-
-  // Received Packets
-  public ArrayList<Packet> getReceivedPackets() {
-    return receivePackets;
-  }
-
-  // Tick Method
-  public void tick() {
-    while (!packets.isEmpty()) {
-      Packet packet = packets.poll();
-      if (packet.getDestination().getIpAddress().equals(this.ipAddress))
-        receivePackets.add(packet);
-      else
-        default_gateway.addPacket(packet, this);
+	public ArrayList<Packet> getReceivedPackets() {
+		return receivedPackets;
+	}
+	
+	// Connection methods
+	public void addConnection(Connection connection) {
+        if (connection.getNode1().getId() == this.id) {
+            this.connections.add(connection);
+        } else if (connection.getNode2().getId() == this.id) {
+            Connection reversedConnection = new Connection(this, connection.getNode1(), connection.getLatency());
+            this.connections.add(reversedConnection);
+        } else {
+            throw new IllegalArgumentException("Connection does not involve this node.");
+        }
     }
-  }
+	
+	public void removeConnection(Connection connection) {
+		connections.remove(connection);
+	}
+	
+	// Packet methods
+	public void addPacket(Packet packet) {
+		packets.add(packet);
+	}
+	
+	public void removePacket(Packet packet) {
+		packets.remove(packet);
+	}
+	
+	public abstract ArrayList<Node> routePacket(Packet packet);
+	
+	public void sendPacket(Packet packet) {
+		ArrayList<Node> nextHops = routePacket(packet);
+		if (nextHops.isEmpty()) {
+			System.out.println("No route found for packet\nCannot send\nDropping packet");
+			removePacket(packet);
+            return;
+		}
+		
+		for (Node nextHop : nextHops) {
+            for (Connection connection : connections) {
+                if (connection.getNode2().getId() == nextHop.getId()) {
+                    System.out.println("Sending packet from " + this.name + " to " + nextHop.name);
+                    connection.addPacket(packet); // transmit packet to a connection
+                    break;
+                }
+            }
+        }
+	}
+	
+	// Received Packet methods
+	public void addReceivedPacket(Packet packet) {
+		receivedPackets.add(packet);
+	}
+	
+	public ArrayList<Node> getNeighbors() {
+		ArrayList<Node> neighbors = new ArrayList<Node>();
+		for (Connection conn : this.connections) {
+			neighbors.add(conn.getNode2());
+		}
+		return neighbors;
+	}
 }

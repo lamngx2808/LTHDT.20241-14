3e383cea78f9010e58b5b28b1395757899e1757c
lexuankyxd <lexuankyxd@gmail.com>
2024-12-24 22:57:28 +0700

    updated router class

--- sourcecode/PacketRouting/Router.java
+++ sourcecode/PacketRouting/Router.java
@@ -1,40 +1,39 @@
 package PacketRouting;
 
+import java.util.ArrayList;
 import java.util.Queue;
 
 public class Router extends Node {
   private RoutingAlgorithm routingAlgorithm;
-
+  private ArrayList<RoutingEntry> routingTable = new ArrayList<>();
   // Constructor
   public Router(String name, String ip) {
     super(name, ip);
   }
 
   @Override
-  public void tick() {
-    Queue<Packet> packets = this.getPackets();
-
-    while (!packets.isEmpty()) {
-      Packet packet = packets.poll();
-
-      if (packet.getDestination().getIpAddress().equals(this.getIpAddress())) {
-        this.getReceivedPackets().add(packet);
-      } else {
-        boolean found = false;
-
-        for (RoutingEntry re : getRoutingTable()) {
-          if (re.getDestination().equals(packet.getDestination().getIpAddress())) {
-            re.getConnection().tick();
-            re.getConnection().addPacket(packet, this);
-            found = true;
-            break;
-          }
-        }
-
-        if (!found) {
-          this.getDefault_gateway().addPacket(packet, this);
-        }
+  public ArrayList<Node> routePacket(Packet packet) {
+    ArrayList<Node> nodes = new ArrayList<>();
+    for(RoutingEntry entry : routingTable) {
+      if(packet.getDestination().equals(entry.getDestination())) {
+        nodes.add(entry.getConnection().getNode1() == this ? entry.getConnection().getNode2() : entry.getConnection().getNode1());
       }
     }
+    return nodes;
+  }
+
+  public void addEntry(RoutingEntry entry) {
+    routingTable.add(entry);
+  }
+  public void removeEntry(RoutingEntry entry) {
+    routingTable.remove(entry);
+  }
+  public void updateEntry(RoutingEntry old, RoutingEntry n){
+    routingTable.remove(old);
+    routingTable.add(n);
+  }
+
+  public ArrayList<RoutingEntry> getRoutingTable() {
+    return routingTable;
   }
 }

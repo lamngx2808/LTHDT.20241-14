f9481c050fda9ea492e05359d8cc9e6f1fc64084
20194597_NguyenTheKien <121213626+NguyenTheKien@users.noreply.github.com>
2024-12-25 18:22:49 +0700

    fix package

---
+++ DraggableComponent.java
@@ -0,0 +1,63 @@
+package PacketRouting.GUI;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class DraggableComponent extends JLabel {
+
+  private volatile int screenX = 0;
+  private volatile int screenY = 0;
+  private volatile int myX = 0;
+  private volatile int myY = 0;
+  String name;
+  String ipAddress;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getIpAddress() {
+    return ipAddress;
+  }
+
+  public void setIp(String ip) {
+    this.ipAddress = ip;
+  }
+
+  public DraggableComponent(String name, String ip) {
+    this.name = name;
+    this.ipAddress = ip;
+    setBounds(0, 0, 64, 41);
+    setIcon(new ImageIcon("router.png"));
+
+    addMouseListener(new MouseAdapter() {
+      @Override
+      public void mousePressed(MouseEvent e) {
+        screenX = e.getXOnScreen();
+        screenY = e.getYOnScreen();
+
+        myX = getX();
+        myY = getY();
+      }
+
+    });
+    addMouseMotionListener(new MouseMotionAdapter() {
+
+      @Override
+      public void mouseDragged(MouseEvent e) {
+        int deltaX = e.getXOnScreen() - screenX;
+        int deltaY = e.getYOnScreen() - screenY;
+
+        setLocation(myX + deltaX, myY + deltaY);
+        repaint();
+      }
+    });
+  }
+
+}
